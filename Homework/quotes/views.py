7d1dc2f568a81d90396bf9f430545de0569ff5db
Nikita200 <Ponomariovnikita02@gmail.com>
2026-03-12 14:39:06 +0100

    here new templetes code and also views)

--- Homework/quotes/views.py
+++ Homework/quotes/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Quote
+
 
 def main(request):
 
-    quotes = Quote.objects.all()
+    quotes_list = Quote.objects.all()
+
+    paginator = Paginator(quotes_list, 5)   # 5 quotes per page
+
+    page_number = request.GET.get("page")
+
+    quotes = paginator.get_page(page_number)
 
     return render(request, "quotes/index.html", {"quotes": quotes})
