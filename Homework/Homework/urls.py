5675849fec5f1143efa51c7d7551e35cd55158e6
Nikita200 <Ponomariovnikita02@gmail.com>
2026-03-13 13:56:51 +0100

    small update

--- Homework/Homework/urls.py
+++ Homework/Homework/urls.py
@@ -1,3 +1,4 @@
+"""
 
 from django.contrib import admin
 from django.urls import path, include
@@ -6,4 +7,17 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('quotes.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+]"""
+from django.contrib import admin
+from django.urls import path, include
+from quotes import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.main, name='main'),
+    path('tag/<str:tag_name>/', views.tag_search, name='tag'),
+    path('register/', views.register, name='register'),
+    path('add/', views.add_quote, name='add_quote'),
+
+    path('accounts/', include('django.contrib.auth.urls')),  # ВАЖЛИВО
 ]
